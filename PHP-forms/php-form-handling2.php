dfa669acbc9447da115519e7ef62231084090b62
WesleyNix1993 <wesleynix7@gmail.com>
2023-01-11 16:12:17 +0100

    PHP (#2)

---
+++ PHP-forms/php-form-handling2.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="https://api.dev-master.ninja/html/form" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
