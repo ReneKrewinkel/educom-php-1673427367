b870f33b19be3761f6d16575160eb92332ceeeab
WesleyNix1993 <wesleynix7@gmail.com>
2023-01-11 11:23:18 +0100

    PHP (#1)

--- php-math.php
+++ php-math.php
@@ -1,8 +1,29 @@
 <?php
-echo(pi());
+echo(pi()).  "<br>";
 ?>
 
 <?php
-echo(min(0, 150 ,30 ,20, -8, -200));
-echo(max(0, 150 ,30 ,20, -8, -200));
+echo(min(0, 150 ,30 ,20, -8, -200) .  "<br>");
+echo(max(0, 150 ,30 ,20, -8, -200)).  "<br>";
+?>
+
+<?php
+echo(abs(-6.7)).  "<br>";
+?>
+
+<?php
+echo(sqrt(64)).  "<br>";
+?>
+
+<?php
+echo(round(0.60)).  "<br>";
+echo(round(0.49)).  "<br>";
+?>
+
+<?php
+echo(rand()).  "<br>";
+?>
+
+<?php
+echo(rand(10,100)).  "<br>";
 ?>
