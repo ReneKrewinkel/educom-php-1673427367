9248969d04e93da04f75d71cd759d9b8449ddc6d
WesleyNix1993 <wesleynix7@gmail.com>
2023-01-11 14:11:28 +0100

    PHP (#1)

---
+++ php-arrays.php
@@ -0,0 +1,14 @@
+<?php
+$cars = array("Volvo", "BMW", "Mercedes");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
+
+<?php
+$cars = array("Volvo", "BMW", "Mercedes");
+echo count($cars);
+?>
+
+<?php
+/* Exercise */
+$fruits = array("Apple", "Banana", "Orange");
+echo count($fruits);
