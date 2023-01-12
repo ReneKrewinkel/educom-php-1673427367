50d579a28c2e617a59ad49141b1eac090254be85
WesleyNix1993 <wesleynix7@gmail.com>
2023-01-12 10:08:01 +0100

    PHP (#3)

---
+++ PHP-advanced/php-file-open-read-close.php
@@ -0,0 +1,28 @@
+<?php
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+echo fread($myfile, filesize("webdictionary.txt"));
+fclose($myfile);
+?>
+
+<?php
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+echo fgets($myfile);
+fclose($myfile);
+?>
+
+<?php
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+while(!feof($myfile)){
+    echo fgets($myfile) . "<br>";
+}
+fclose($myfile);
+?>
+
+<?php
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+// Output one character until end-of-file
+while(!feof($myfile)) {
+  echo fgetc($myfile);
+}
+fclose($myfile);
+?>
